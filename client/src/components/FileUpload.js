1e55022f471b3f94ce58a959d73a7fe667f00f4d
Thalía Zapata <thaly18.98@gmail.com>
2020-08-21 18:22:53 -0500

    Produccion

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,3 +1,4 @@
+import { Container, Main } from '../styles/FileUpload';
 import React, { Fragment, useState } from 'react';
 
 import Message from './Message';
@@ -53,38 +54,33 @@ const FileUpload = () => {
   };
 
   return (
-    <Fragment>
-      {message ? <Message msg={message} /> : null}
-      <form onSubmit={onSubmit}>
-        <div className="custom-file mb-4">
-          <input
-            type="file"
-            className="custom-file-input"
-            id="customFile"
-            onChange={onChange}
-          />
-          <label className="custom-file-label" htmlFor="customFile">
-            {filename}
-          </label>
-        </div>
-
-        <Progress percentage={uploadPercentage} />
-
-        <input
-          type="submit"
-          value="Upload"
-          className="btn btn-primary btn-block mt-4"
-        />
-      </form>
-      {/* {uploadedFile ? (
-        <div className="row mt-5">
-          <div className="col-md-6 m-auto">
-            <h3 className="text-center">{uploadedFile.fileName}</h3>
-            <img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
+    <Container>
+      <Main>
+        {message ? <Message msg={message} /> : null}
+        <form onSubmit={onSubmit}>
+          <div className="custom-file mb-4">
+            <input
+              type="file"
+              accept=".mp4"
+              className="custom-file-input"
+              id="customFile"
+              onChange={onChange}
+            />
+            <label className="custom-file-label" htmlFor="customFile">
+              {filename}
+            </label>
           </div>
-        </div>
-      ) : null} */}
-    </Fragment>
+
+          <Progress percentage={uploadPercentage} />
+
+          <input
+            type="submit"
+            value="Upload"
+            className="btn btn-primary btn-block mt-4"
+          />
+        </form>
+      </Main>
+    </Container>
   );
 };
 
